fb865906c6f12889eadb69700840432376b73303
Minh Pham <quanbinh15101108@gmail.com>
2020-04-28 09:08:22 +0700

    no
    no

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TaskList from './TaskList';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<TaskList />, document.getElementById('root'));
